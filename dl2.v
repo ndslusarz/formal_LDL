001f3d7f4673278b0a1ce9757de7df2beeb641a2
Alessandro Bruni <brun@itu.dk>
2024-02-11 18:03:14 +0100

    Cleaning up the repository Splitting into multiple files

---
+++ dl2.v
@@ -0,0 +1,264 @@
+From HB Require Import structures.
+Require Import Coq.Program.Equality.
+From mathcomp Require Import all_ssreflect all_algebra.
+From mathcomp Require Import lra.
+From mathcomp Require Import all_classical.
+From mathcomp Require Import reals ereal signed.
+From mathcomp Require Import topology derive normedtype sequences
+ exp measure lebesgue_measure lebesgue_integral hoelder.
+Require Import util ldl.
+
+Import Num.Def Num.Theory GRing.Theory.
+Import Order.TTheory.
+Import numFieldTopology.Exports.
+
+(* Ale: disabled for now
+Section shadow_lifting_dl2_and.
+Context {R : realType}.
+Variable M : nat.
+Hypothesis M0 : M != 0%N.
+
+Lemma shadowlifting_dl2_andE (p : R) : p > 0 ->
+  forall i, ('d (@dl2_and _ M.+1) '/d i) (const_mx p) = 1.
+Proof.
+move=> p0 i.
+rewrite /partial.
+have /cvg_lim : h^-1 * (dl2_and (const_mx p + h *: err_vec i) -
+                        dl2_and (n:=M.+1) (const_mx p))
+       @[h --> (0:R)^'] --> (1:R)%R.
+  rewrite /dl2_and.
+  have H : forall h, h != 0 ->
+      \sum_(x < M.+1) (const_mx p + h *: err_vec i) ``_ x -
+      \sum_(x < M.+1) (const_mx (n:=M.+1) (m:=1) p) ``_ x = h.
+    move=> h h0; rewrite [X in X - _](bigD1 i)//= !mxE eqxx mulr1.
+    rewrite (eq_bigr (fun=> p)); last first.
+      by move=> j ji; rewrite !mxE eq_sym (negbTE ji) mulr0 addr0.
+    rewrite [X in _ - X](eq_bigr (fun=> p)); last by move=> *; rewrite mxE.
+    rewrite [X in _ - X](bigD1 i)//= (addrC p h) -addrA.
+    by rewrite addrA -(addrA h) addrK.
+  have : h^-1 * h @[h --> (0:R)^'] --> (1:R)%R.
+    have : {near (0:R)^', (fun=> 1) =1 (fun h => h^-1 * h)}.
+      near=> h; rewrite mulVf//.
+      by near: h;  exact: nbhs_dnbhs_neq.
+    by move/near_eq_cvg/cvg_trans; apply; exact: cvg_cst.
+  apply: cvg_trans; apply: near_eq_cvg => /=; near=> k.
+  by rewrite H//; near: k; exact: nbhs_dnbhs_neq.
+by apply; exact: Rhausdorff.
+Unshelve. all: by end_near. Qed.
+
+Corollary shadow_lifting_dl2_and :  shadow_lifting (@dl2_and R M.+1).
+Proof. by move=> p p0 i; rewrite shadowlifting_dl2_andE. Qed.
+
+End shadow_lifting_dl2_and.
+*)
+
+Section dl2_lemmas.
+Local Open Scope ldl_scope.
+Local Open Scope ring_scope.
+Context {R : realType}.
+Variable p : R.
+
+Local Notation "[[ e ]]_dl2" := (@dl2_translation R _ e).
+
+Lemma dl2_andC (e1 e2 : expr Bool_N) : [[ e1 `/\ e2 ]]_dl2 = [[ e2 `/\ e1 ]]_dl2.
+Proof.
+by rewrite /=/sumE ?big_cons ?big_nil /= adde0 adde0 addeC.
+Qed.
+
+Lemma dl2_andA (e1 e2 e3 : expr Bool_P) :
+  [[ e1 `/\ (e2 `/\ e3) ]]_dl2 = [[ (e1 `/\ e2) `/\ e3 ]]_dl2.
+Proof.
+by rewrite /=/sumE ?big_cons ?big_nil !adde0 addeA.
+Qed.
+
+Lemma dl2_orC (e1 e2 : expr Bool_P) :
+ [[ e1 `\/ e2 ]]_dl2 = [[ e2 `\/ e1 ]]_dl2.
+Proof.
+rewrite /= !big_cons big_nil !mule1; congr *%E.
+by rewrite muleC.
+Qed.
+
+Lemma dl2_orA (e1 e2 e3 : expr Bool_P) :
+  [[ e1 `\/ (e2 `\/ e3) ]]_dl2 = [[ (e1 `\/ e2) `\/ e3 ]]_dl2.
+Proof.
+rewrite /=.
+rewrite !big_cons big_nil !mule1.
+congr (_ * _)%E.
+rewrite muleCA.
+by rewrite !muleA.
+Qed.
+
+Lemma dl2_translation_le0 e :
+  ([[ e ]]_dl2 <= 0 :> dl2_type_translation Bool_P)%E.
+Proof.
+dependent induction e using expr_ind' => /=.
+- by case: b.
+- rewrite /sumE big_map big_seq sume_le0// => t tl.
+  move/List.Forall_forall : H => /(_ t); apply => //.
+  exact/In_in.
+- rewrite big_map big_seq; have [ol|ol] := boolP (odd (length l)).
+    rewrite exprS -signr_odd ol expr1 mulrN1 !EFinN oppeK mul1e.
+    have [l0|l0] := pselect (forall i, i \in l -> [[i]]_dl2 != 0)%E; last first.
+      move/existsNP : l0 => [/= x /not_implyP[xl /negP/negPn/eqP x0]].
+      rewrite le_eqVlt; apply/orP; left.
+      rewrite prode_seq_eq0; apply/hasP; exists x => //.
+      by rewrite xl x0 eqxx.
+    apply/ltW/sgeN1_lt0; rewrite -big_seq prodeN1.
+      by rewrite -signr_odd ol expr1.
+    move=> e el; rewrite lt_neqAle l0//=.
+    by move/List.Forall_forall : H => /(_ e); apply => //; exact/In_in.
+  rewrite exprS -signr_odd (negbTE ol) expr0 mulN1r.
+  rewrite EFinN mulN1e oppe_le0.
+  have [l0|l0] := pselect (forall i, i \in l -> [[i]]_dl2 != 0)%E; last first.
+    move/existsNP : l0 => [/= x /not_implyP[xl /negP/negPn/eqP x0]].
+    rewrite le_eqVlt; apply/orP; left.
+    rewrite eq_sym prode_seq_eq0; apply/hasP; exists x => //.
+    by rewrite xl x0 eqxx.
+  apply/ltW/sge1_gt0; rewrite -big_seq prodeN1.
+    by rewrite -signr_odd (negbTE ol) expr0.
+  move=> e el; rewrite lt_neqAle l0//=.
+  by move/List.Forall_forall : H => /(_ e); apply => //; exact/In_in.
+- case: c => //=.
+  by rewrite lee_fin oppr_le0 le_maxr lexx orbT.
+Qed.
+
+Definition is_dl2 b (x : \bar R) := (if b then x == 0 else x < 0)%E.
+
+Lemma dl2_nary_inversion_andE1 (s : seq (expr (Bool_P))) :
+  is_dl2 true ([[ and_E s ]]_dl2) ->
+  (forall i, (i < size s)%N -> is_dl2 true ([[ nth (Bool _ false) s i ]]_dl2)).
+Proof.
+rewrite/is_dl2.
+elim: s => //= h t ih H [_|]/=.
+  move: H; rewrite /sumE big_cons.
+  rewrite nadde_eq0//.
+  - by move=> /andP[->].
+  - exact: dl2_translation_le0.
+  - rewrite big_seq_cond; apply: sume_le0 => /= x.
+    by rewrite andbT => /mapP[/= e et] ->; exact: dl2_translation_le0.
+move=> n; rewrite ltnS => nt /=; apply: ih => //.
+move: H; rewrite /sumE big_cons.
+rewrite nadde_eq0.
+- by move=> /andP[_ ->].
+- exact: dl2_translation_le0.
+- rewrite big_seq_cond; apply: sume_le0 => /= x.
+  by rewrite andbT => /mapP[/= e et] ->; exact: dl2_translation_le0.
+Qed.
+
+Lemma dl2_nary_inversion_andE0 (s : seq (expr (Bool_P))) :
+  is_dl2 false ([[ and_E s ]]_dl2) ->
+  (exists i, (is_dl2 false ([[ nth (Bool _ false) s i ]]_dl2)) && (i < size s)%nat).
+Proof.
+rewrite/is_dl2.
+elim: s => [|h t ih] //=; first by rewrite /sumE big_nil ltxx.
+rewrite /sumE big_cons => /nadde_lt0 => /(_ (dl2_translation_le0 _)).
+have : (\sum_(j <- [seq [[i]]_dl2 | i <- t]) j <= 0)%E.
+  rewrite big_seq_cond; apply: sume_le0 => /= z.
+  by rewrite andbT => /mapP[/= e et ->]; exact: dl2_translation_le0.
+move=> /[swap] /[apply] /orP[H|/ih[j /andP[j0 jt]]].
+  by exists 0%N; rewrite /= H.
+by exists j.+1; rewrite /= j0.
+Qed.
+
+Lemma dl2_nary_inversion_orE1 (s : seq (expr (Bool_P))) : [[ or_E s ]]_dl2 = 0%E ->
+  exists i, ([[ nth (Bool _ false) s i ]]_dl2 == 0%E) && (i < size s)%nat.
+Proof.
+elim: s => [|h t ih] /=.
+  rewrite big_nil mule1 expr1 EFinN => /eqe_oppLRP.
+  by rewrite oppe0 => /eqP; rewrite onee_eq0.
+move=> /eqP; rewrite mule_eq0 eqe signr_eq0/=.
+rewrite big_cons mule_eq0 => /orP[H|/eqP H].
+  by exists 0%N; rewrite /= H.
+have /ih[j /andP[Hj jt]] : [[or_E t]]_dl2 = 0 by rewrite /= H mule0.
+by exists j.+1; rewrite /= Hj.
+Qed.
+
+Lemma dl2_nary_inversion_orE0 (Es : seq (expr (Bool_P)) ) :
+    ([[ or_E Es ]]_dl2  < 0)%E  -> (forall i, (i < size Es)%nat -> ([[ nth (Bool _ false) Es i ]]_dl2 < 0)%E).
+Proof.
+elim: Es => //= a l IH.
+rewrite big_cons muleCA mule_lt0 => /andP[aneq0]/andP[]/[swap] _.
+rewrite exprS EFinM -muleA EFinN mulN1e oppe_eq0 => lneq0.
+have ale0 := dl2_translation_le0 a.
+have alt0 : ([[a]]_dl2 < 0)%E by rewrite lt_neqAle aneq0 ale0.
+elim => [_//=|i _].
+rewrite ltnS => isize.
+apply IH => //.
+rewrite lt_neqAle lneq0/= big_map.
+apply: prode_le0 => j.
+exact: dl2_translation_le0.
+Qed.
+
+Lemma dl2_translations_Vector_coincide: forall n (e : @expr R (Vector_T n)),
+  [[ e ]]_dl2 = [[ e ]]b.
+Proof.
+dependent induction e => //=.
+dependent destruction e1.
+by rewrite (IHe2 _ _ e2 erefl JMeq_refl).
+Qed.
+
+Lemma dl2_translations_Index_coincide: forall n (e : expr (Index_T n)),
+  [[ e ]]_dl2 = [[ e ]]b.
+Proof.
+dependent induction e => //=.
+Qed.
+
+Lemma dl2_translations_Real_coincide (e : expr Real_T):
+  [[ e ]]_dl2 = [[ e ]]b.
+Proof.
+dependent induction e => //=;
+rewrite ?(IHe1 e1 erefl JMeq_refl) ?(IHe2 e2 erefl JMeq_refl) ?(IHe e erefl JMeq_refl) //=.
+by rewrite dl2_translations_Vector_coincide dl2_translations_Index_coincide.
+Qed.
+
+Lemma dl2_soundness (e : expr Bool_P) b :
+  is_dl2 b ([[ e ]]_dl2) -> [[ e ]]b = b.
+Proof.
+dependent induction e using expr_ind'.
+- move: b b0 => [] [] //=.
+  by rewrite lt_irreflexive.
+- rewrite List.Forall_forall in H.
+  move: b => [].
+  + move/dl2_nary_inversion_andE1.
+    rewrite [bool_translation (and_E l)]/= foldrE big_map big_seq big_all_cond => h.
+    apply: allT => x/=.
+    apply/implyP => /nthP xnth.
+    have [i il0 <-] := xnth (Bool _ false).
+    apply: H => //. rewrite ?h// -In_in mem_nth//.
+    by rewrite h.
+  + move/dl2_nary_inversion_andE0.
+    rewrite [bool_translation (and_E l)]/= foldrE big_map big_all.
+    elim=>// i /andP[/eqP i0 isize].
+    apply/allPn; exists (nth (Bool _ false) l i); first by rewrite mem_nth.
+    apply/negPf; apply: H => //.
+    * by rewrite -In_in mem_nth.
+    * rewrite /is_dl2/=. move: i0.
+      by rewrite eqb_id.
+- rewrite List.Forall_forall in H.
+  move: b => [].
+  + rewrite/is_dl2=>/eqP; move/dl2_nary_inversion_orE1.
+    rewrite [bool_translation (or_E l)]/= foldrE big_map big_has.
+    elim=>// i /andP[/eqP i0 isize].
+    apply/hasP; exists (nth (Bool _ false) l i); first by rewrite mem_nth.
+    apply: H => //.
+    by rewrite -In_in mem_nth.
+    rewrite /is_dl2/=. by rewrite i0.
+  + move/dl2_nary_inversion_orE0.
+    rewrite [bool_translation (or_E l)]/= foldrE big_map big_has => h.
+    apply/hasPn => x.
+    move/nthP => xnth.
+    have [i il0 <-] := xnth (Bool _ false).
+    apply/negPf; apply: H => //.
+    * by rewrite ?h// -In_in mem_nth.
+    * by rewrite /is_dl2/= h.
+- case: c; rewrite //=; rewrite -!dl2_translations_Real_coincide;
+  set t1 := _ e1; set t2 := _ e2; case: b => //.
+  + by rewrite/is_dl2=>/eqP; move/maxr0_le; rewrite subr_le0.
+  + rewrite/is_dl2 lte_fin oppr_lt0 /maxr; case: ifPn; first by rewrite lt_irreflexive.
+    by rewrite subr_gt0 => _; move/lt_geF.
+  + by rewrite/is_dl2=>/eqP; case=>/eqP; rewrite oppr_eq0 normr_eq0 subr_eq0.
+  + rewrite/is_dl2; rewrite lte_fin oppr_lt0 normr_gt0.
+    by rewrite subr_eq0; move/eqP => h; apply/eqP.
+Qed.
+
+End dl2_lemmas.
---
+++ ldl.v
@@ -0,0 +1,468 @@
+From HB Require Import structures.
+Require Import Coq.Program.Equality.
+From mathcomp Require Import all_ssreflect all_algebra.
+From mathcomp Require Import lra.
+From mathcomp Require Import all_classical.
+From mathcomp Require Import reals ereal signed.
+From mathcomp Require Import topology derive normedtype sequences
+ exp measure lebesgue_measure lebesgue_integral hoelder.
+Require Import util.
+
+(**md**************************************************************************)
+(* # Logics                                                                   *)
+(*                                                                            *)
+(* ## Definitions                                                             *)
+(* - err_vec i with i : 'I_n.+1                                               *)
+(*   a vector $\delta_i$ with $1$ at index $i$ and $0$ elsewhere              *)
+(* - ('d f '/d i) a with f : rV[R]_n.+1 -> R                                  *)
+(*   $\lim_{h\to 0, h\neq 0} \frac{f(a + h\delta_i) - f(a)}{h}$               *)
+(* - shadow_lifting f with f : rV[R]_n.+1 -> R                                *)
+(*   $\forall p, p > 0 \to \forall i, \frac{d\,f}{d\,x_i} [p; \cdots; p] > 0$ *)
+(* - product_and v with v : 'rV_n                                             *)
+(*   $\Pi_{i < n} v_i$                                                        *)
+(* - dl2_and v with v : 'rV_n                                                 *)
+(*   $\sum_{i < n} v_i$                                                       *)
+(*                                                                            *)
+(* ## Properties                                                              *)
+(* - satisfy shadow_lifting:                                                  *)
+(*   + product_and                                                            *)
+(*   + dl2_and                                                                *)
+(*                                                                            *)
+(******************************************************************************)
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Import Num.Def Num.Theory GRing.Theory.
+Import Order.TTheory.
+Import numFieldTopology.Exports.
+Local Open Scope classical_set_scope.
+
+Reserved Notation "{[ e ]}" (format "{[  e  ]}").
+Reserved Notation "[[ e ]]b" (at level 10, format "[[  e  ]]b").
+Reserved Notation "[[ e ]]_ l" (at level 10, format "[[ e ]]_ l").
+Reserved Notation "nu .-[[ e ]]_stl" (at level 10, format "nu .-[[ e ]]_stl").
+Reserved Notation "[[ e ]]_dl2" (at level 10, format "[[ e ]]_dl2").
+
+Inductive simple_type :=
+| Bool_T of bool
+| Index_T of nat
+| Real_T
+| Vector_T of nat
+| Network_T of nat & nat.
+
+Definition Bool_P := Bool_T false.
+Definition Bool_N := Bool_T true.
+
+Inductive comparisons : Type :=
+| le_E : comparisons
+| eq_E : comparisons.
+
+Section expr.
+Context {R : realType}.
+
+Inductive expr : simple_type -> Type :=
+  | Real : R -> expr Real_T
+  | Bool : forall x, bool -> expr (Bool_T x)
+  | Index : forall n : nat, 'I_n -> expr (Index_T n)
+  | Vector : forall n : nat, n.-tuple R -> expr (Vector_T n)
+
+  (*logical connectives*)
+  | and_E : forall x, seq (expr (Bool_T x)) -> expr (Bool_T x)
+  | or_E : forall x, seq (expr (Bool_T x)) -> expr (Bool_T x)
+  | not_E : expr (Bool_N) -> expr (Bool_N)
+
+  (*arithmetic operations*)
+  | add_E : expr Real_T -> expr Real_T -> expr Real_T
+  | mult_E : expr Real_T -> expr Real_T -> expr Real_T
+  | minus_E : expr Real_T -> expr Real_T
+
+  (* networks and applications *)
+  | net : forall n m : nat, (n.-tuple R -> m.-tuple R) -> expr (Network_T n m)
+  | app_net : forall n m : nat, expr (Network_T n m) -> expr (Vector_T n) -> expr (Vector_T m)
+  | lookup_E n: expr (Vector_T n) -> expr (Index_T n) -> expr Real_T
+
+  (*comparisons*)
+  | comparisons_E : forall x, comparisons -> expr Real_T -> expr Real_T -> expr (Bool_T x).
+
+End expr.
+
+HB.instance Definition _ (R : realType) b :=
+  @gen_eqMixin (@expr R (Bool_T b)).
+
+Declare Scope ldl_scope.
+
+Notation "a `/\ b" := (and_E [:: a; b]) (at level 45).
+Notation "a `\/ b" := (or_E [:: a; b]) (at level 45).
+Notation "a `=> b" := (or_E [:: (not_E a); b]) (at level 55).
+Notation "`~ a" := (not_E a) (at level 75).
+Notation "a `+ b" := (add_E a b) (at level 50).
+Notation "a `* b" := (mult_E a b) (at level 40).
+Notation "`- a" := (minus_E a) (at level 45).
+
+Local Open Scope ldl_scope.
+
+Notation "a `<= b" := (comparisons_E _ le_E a b) (at level 70).
+Notation "a `== b" := (comparisons_E _ eq_E a b) (at level 70).
+Notation "a `!= b" := (`~ (a == b)) (at level 70).
+Notation "a `< b" := (a `<= b /\ a `!= b) (at level 70).
+Notation "a `>= b" := (b `<= a) (at level 70).
+Notation "a `> b" := (b `< a) (at level 70).
+
+Lemma expr_ind' (R : realType) :
+  forall P : forall s : simple_type, expr s -> Prop,
+       (forall s : R, P Real_T (Real s)) ->
+       (forall (b x : bool), P (Bool_T x) (Bool x b)) ->
+       (forall (n : nat) (o : 'I_n), P (Index_T n) (Index o)) ->
+       (forall (n : nat) (t : n.-tuple R), P (Vector_T n) (Vector t)) ->
+       (forall b (l : seq (expr (Bool_T b))), List.Forall (fun x => P (Bool_T b) x) l -> P (Bool_T b) (and_E l)) ->
+       (forall b (l : seq (expr (Bool_T b))), List.Forall (fun x => P (Bool_T b) x) l -> P (Bool_T b) (or_E l)) ->
+       (forall e : expr (Bool_T true), P (Bool_T true) e -> P (Bool_T true) (`~ e)) ->
+       (forall e : expr Real_T,
+        P Real_T e -> forall e0 : expr Real_T, P Real_T e0 -> P Real_T (e `+ e0)) ->
+       (forall e : expr Real_T,
+        P Real_T e -> forall e0 : expr Real_T, P Real_T e0 -> P Real_T (e `* e0)) ->
+       (forall e : expr Real_T, P Real_T e -> P Real_T (`- e)) ->
+       (forall (n m : nat) (t : n.-tuple R -> m.-tuple R), P (Network_T n m) (net t)) ->
+       (forall (n m : nat) (e : expr (Network_T n m)),
+        P (Network_T n m) e ->
+        forall e0 : expr (Vector_T n), P (Vector_T n) e0 -> P (Vector_T m) (app_net e e0)) ->
+       (forall (n : nat) (e : expr (Vector_T n)),
+        P (Vector_T n) e ->
+        forall e0 : expr (Index_T n), P (Index_T n) e0 -> P Real_T (lookup_E e e0)) ->
+       (forall (c : comparisons) (e : expr Real_T) b,
+        P Real_T e -> forall e0 : expr Real_T, P Real_T e0 -> P (Bool_T b) (comparisons_E b c e e0)) ->
+       forall (s : simple_type) (e : expr s), P s e.
+Proof.
+move => P H H0 H1 H2 H3 H4 H7 H8 H9 H10 H11 H12 H13 H14 s e.
+revert e.
+revert s.
+fix F1 2.
+intros.
+destruct e.
+  * apply H.
+  * apply H0.
+  * apply H1.
+  * apply H2.
+  * apply H3.
+    induction l.
+    + apply List.Forall_nil.
+    + apply List.Forall_cons_iff.
+      split.
+      - apply F1.
+      - apply IHl.
+  * apply H4.
+    induction l.
+    + apply List.Forall_nil.
+    + apply List.Forall_cons_iff.
+      split.
+      - apply F1.
+      - apply IHl.
+  * apply H7; eauto.
+  * apply H8; eauto.
+  * apply H9; eauto.
+  * apply H10; eauto.
+  * apply H11.
+  * apply H12; eauto.
+  * apply H13; eauto.
+  * apply H14; eauto.
+Qed.
+
+Local Close Scope ldl_scope.
+
+Inductive DL := Lukasiewicz | Yager | Godel | product.
+
+Section type_translation.
+Context {R : realType}.
+
+Definition type_translation (t : simple_type) : Type:=
+  match t with
+  | Bool_T x => R
+  | Real_T => R
+  | Vector_T n => n.-tuple R
+  | Index_T n => 'I_n
+  | Network_T n m => n.-tuple R -> m.-tuple R
+end.
+
+Definition bool_type_translation (t : simple_type) : Type:=
+  match t with
+  | Bool_T x => bool
+  | Real_T => R
+  | Vector_T n => n.-tuple R
+  | Index_T n => 'I_n
+  | Network_T n m => n.-tuple R -> m.-tuple R
+  end.
+
+Definition dl2_type_translation (t : simple_type) : Type :=
+  match t with
+  | Bool_T x => \bar R (* TODO: this should b [-oo,0] *)
+  | Real_T => R
+  | Vector_T n => n.-tuple R
+  | Index_T n => 'I_n
+  | Network_T n m => n.-tuple R -> m.-tuple R
+end.
+
+Definition stl_type_translation (t : simple_type) : Type:=
+  match t with
+  | Bool_T x => \bar R
+  | Real_T => R
+  | Vector_T n => n.-tuple R
+  | Index_T n => 'I_n
+  | Network_T n m => n.-tuple R -> m.-tuple R
+end.
+
+End type_translation.
+
+Section bool_translation.
+Local Open Scope ring_scope.
+Local Open Scope ldl_scope.
+Context {R : realType}.
+
+Fixpoint bool_translation t (e : @expr R t) : bool_type_translation t :=
+  match e in expr t return bool_type_translation t with
+  | Bool b x => x
+  | Real r => r%R
+  | Index n i => i
+  | Vector n t => t
+
+  | and_E b Es => foldr andb true (map (@bool_translation (Bool_T b)) Es)
+  | or_E b Es => foldr orb false (map (@bool_translation (Bool_T b)) Es)
+  | `~ E1 => ~~ << E1 >>
+
+  (* arith *)
+  | E1 `+ E2 => << E1 >> + << E2 >>
+  | E1 `* E2 => << E1 >> * << E2 >>
+  | `- E1 => - << E1 >>
+
+  (*comparisons*)
+  | E1 `== E2 => << E1 >> == << E2 >>
+  | E1 `<= E2 => << E1 >> <= << E2 >>
+  | net n m f => f
+  | app_net n m f v => << f >> << v >>
+  | lookup_E n v i => tnth << v >> << i >>
+  end
+where "<< e >>" := (bool_translation e).
+
+Lemma bool_translation_andE b (Es : seq (expr (Bool_T b))) :
+  bool_translation (and_E Es) = \big[andb/true]_(x <- Es) bool_translation x.
+Proof. by rewrite /= foldrE big_map. Qed.
+
+End bool_translation.
+
+Notation "[[ e ]]b" := (bool_translation e) : ldl_scope.
+
+Section goedel_translation.
+Local Open Scope ring_scope.
+Local Open Scope ldl_scope.
+Context {R : realType}.
+Variables (l : DL) (p : R).
+
+Fixpoint translation t (e: @expr R t) {struct e} : type_translation t :=
+    match e in expr t return type_translation t with
+    | Bool _ true => (1%R : type_translation (Bool_T _))
+    | Bool _ false => (0%R : type_translation (Bool_T _))
+    | Real r => r%R
+    | Index n i => i
+    | Vector n t => t
+
+    | and_E b Es =>
+        match l with
+        | Lukasiewicz => maxr (sumR (map (@translation _) Es)- (size Es)%:R+1) 0
+        | Yager => maxr (1 - ((sumR (map (fun E => (1 - ({[ E ]}: type_translation (Bool_T _)))`^p)%R Es))`^p^-1)) 0
+        | Godel => minR (map (@translation _) Es)
+        | product => prodR (map (@translation _) Es)
+        end
+    | or_E b Es =>
+        match l with
+        | Lukasiewicz => minr (sumR (map (@translation _) Es)) 1
+        | Yager => minr ((sumR (map (fun E => ({[ E ]} : type_translation (Bool_T _))`^p) Es))`^p^-1) 1
+        | Godel => maxR (map (@translation _) Es)
+        | product => product_dl_prodR (map (@translation _) Es)
+        end
+
+    | `~ E1 => 1 - {[ E1 ]}
+
+    (*simple arithmetic*)
+    | E1 `+ E2 => {[ E1 ]} + {[ E2 ]}
+    | E1 `* E2 => {[ E1 ]} * {[ E2 ]}
+    | `- E1 => - {[ E1 ]}
+
+    (*comparisons*)
+    | E1 `== E2 => if {[ E1 ]} == -{[ E2 ]} then ({[ E1 ]} == {[ E2 ]})%:R else maxr (1 - `|({[ E1 ]} - {[ E2 ]}) / ({[ E1 ]} + {[ E2 ]})|) 0
+    | E1 `<= E2 => if {[ E1 ]} == -{[ E2 ]} then ({[ E1 ]} <= {[ E2 ]})%R%:R else maxr (1 - maxr (({[ E1 ]} - {[ E2 ]}) / `|{[ E1 ]} + {[ E2 ]}|) 0) 0
+
+    | net n m f => f
+    | app_net n m f v => (translation f) (translation v)
+    | lookup_E n v i => tnth (translation v) (translation i)
+    end
+where "{[ e ]}" := (translation e).
+
+End goedel_translation.
+
+Section dl2_translation.
+Local Open Scope ereal_scope.
+Local Open Scope ldl_scope.
+Context {R : realType}.
+
+Fixpoint dl2_translation t (e : @expr R t) {struct e} : dl2_type_translation t :=
+    match e in expr t return dl2_type_translation t with
+    | Bool _ true => 0
+    | Bool _ false => -oo
+    | Real r => r
+    | Index n i => i
+    | Vector n t => t
+
+    | and_E _ Es => sumE (map (@dl2_translation _) Es)
+    | or_E _ Es => ((- 1) ^+ (length Es).+1)%:E *
+                 \big[*%E/1%E]_(i <- map (@dl2_translation _) Es) i
+    | `~ E1 => (+oo)%E (* FIX: this case is not covered by DL2 *)
+
+    (*simple arithmetic*)
+    | E1 `+ E2 => ({[ E1 ]} + {[ E2 ]})%R
+    | E1 `* E2 => ({[ E1 ]} * {[ E2 ]})%R
+    | `- E1 => (- {[ E1 ]})%R
+
+    (*comparisons*)
+    | E1 `== E2 => (- `| {[ E1 ]} - {[ E2 ]}|)%:E
+    | E1 `<= E2 => (- maxr ({[ E1 ]} - {[ E2 ]}) 0)%:E
+
+    | net n m f => f
+    | app_net n m f v => {[ f ]} {[ v ]}
+    | lookup_E n v i => tnth {[ v ]} {[ i ]}
+    end
+where "{[ e ]}" := (dl2_translation e).
+
+End dl2_translation.
+
+Section stl_translation.
+Local Open Scope ereal_scope.
+Local Open Scope ldl_scope.
+Context {R : realType}.
+Variables (p : R) (nu : R).
+Hypothesis p1 : (1 <= p)%R.
+Hypothesis nu0 : (0 < nu)%R.
+
+Fixpoint stl_translation t (e: expr t) : stl_type_translation t :=
+    match e in expr t return stl_type_translation t with
+    | Bool _ true => +oo
+    | Bool _ false => -oo
+    | Real r => r
+    | Index n i => i
+    | Vector n t => t
+
+    | and_E _ Es =>
+        let A := map (@stl_translation _) Es in
+        let a_min: \bar R := foldr mine (+oo) A in
+        let a'_i (a_i: \bar R) := (a_i - a_min) * (fine a_min)^-1%:E in
+        if a_min == -oo then -oo
+        else if a_min == +oo then +oo
+        else if a_min < 0 then
+          sumE (map (fun a => a_min * expeR (a'_i a) * expeR (nu%:E * a'_i a)) A) *
+          (fine (sumE (map (fun a => expeR (nu%:E * a'_i a)) A)))^-1%:E
+        else if a_min > 0 then
+          sumE (map (fun a => a * expeR (-nu%:E * a'_i a)) A) *
+          (fine (sumE (map (fun a => expeR (nu%:E * (a'_i a))) A)))^-1%:E
+             else 0
+    | or_E _ Es => (* TODO: double check *)
+        let A := map (@stl_translation _) Es in
+        let a_max: \bar R := - (foldr maxe (+oo)%E A) in
+        let a'_i (a_i: \bar R) := (- a_i - a_max) * (fine a_max)^-1%:E  in
+        if a_max == -oo then -oo
+        else if a_max == +oo then +oo
+        else if a_max < 0 then
+          sumE (map (fun a => a_max * expeR (a'_i a) * expeR (nu%:E * a'_i a)) A) *
+          (fine (sumE (map (fun a => expeR (nu%:E * (a'_i a))) A)))^-1%:E
+        else if a_max > 0 then
+          sumE (map (fun a => a * expeR (-nu%:E * (a'_i a))) A) *
+          (fine (sumE (map (fun a => expeR (nu%:E * (a'_i a))) A)))^-1%:E
+             else 0
+    | `~ E1 => (- {[ E1 ]})%E
+
+    (*simple arithmetic*)
+    | E1 `+ E2 => ({[ E1 ]} + {[ E2 ]})%R
+    | E1 `* E2 => ({[ E1 ]} * {[ E2 ]})%R
+    | `- E1 => (- {[ E1 ]})%R
+
+    (*comparisons*)
+    | E1 `== E2 => (- `| {[ E1 ]} - {[ E2 ]}|)%:E
+    | E1 `<= E2 => ({[ E2 ]} - {[ E1 ]})%:E(* (- maxr ({[ E1 ]} - {[ E2 ]}) 0)%:E *)
+
+    | net n m f => f
+    | app_net n m f v => {[ f ]} {[ v ]}
+    | lookup_E n v i => tnth {[ v ]} {[ i ]}
+    end
+where "{[ e ]}" := (stl_translation e).
+
+End stl_translation.
+
+Notation "nu .-[[ e ]]_stl" := (stl_translation nu e) : ldl_scope.
+Notation "[[ e ]]_dl2" := (dl2_translation e) : ldl_scope.
+
+(* Ale: disabled for now
+
+(* this is already in MCA master *)
+#[global] Hint Extern 0 (Filter (nbhs _^'+)) =>
+  (apply: at_right_proper_filter) : typeclass_instances.
+
+#[global] Hint Extern 0 (Filter (nbhs _^'-)) =>
+  (apply: at_left_proper_filter) : typeclass_instances.
+
+Definition shadow_lifting {R : realType} n (f : 'rV_n.+1 -> R) :=
+  forall p, p > 0 -> forall i, ('d f '/d i) (const_mx p) > 0.
+
+(*Definition shadow_lifting {R : realType} (f : forall n, 'rV_n.1 -> R) :=
+  (* forall Es : seq R, forall i : 'I_(size Es),
+    (* (forall i, nth 0 Es i != 0) -> *) *)
+    forall Es : seq R, forall i : 'I_(size Es), forall e : R,
+    e != 0 (* (0 < e <= 1)  *)->
+    0 < nth 0 Es i <= 1 ->
+    (forall j, j != i -> nth 0 Es j = e) ->
+    partial (f (size Es)) i (row_of_seq Es) > 0.
+
+Lemma all_0_product_partial {R : realType} (Es : seq R) (i : 'I_(size Es)) :
+  partial 0 i (row_of_seq Es) = 0.
+(*I'm not sure if I don't need an additional assumption here*)
+Proof.
+apply/cvg_lim; first exact: Rhausdorff.
+rewrite [X in X @ _ --> _](_ : _ = 0); first exact: (@cvg_cst R).
+by apply/funext => r/=; rewrite /GRing.zero/=(*NB: I shouldn't do that*) subrr mulr0.
+Qed.
+*)
+(*
+About realfun.left_right_continuousP. *)
+
+Definition row_of_seq {R : numDomainType} (s : seq R) : 'rV[R]_(size s) :=
+  (\row_(i < size s) tnth (in_tuple s) i).
+
+Definition dotmul {R : ringType} n (u v : 'rV[R]_n) : R := (u *m v^T)``_0.
+Reserved Notation "u *d w" (at level 40).
+Local Notation "u *d w" := (dotmul u w).
+
+(* NB(rei): WIP *)
+Definition gradient {R : realType} (n : nat) (f : 'rV_n.+1 -> R) a :=
+  \row_(i < n.+1) ('d f '/d i) a.
+
+(* NB(rei): main property of gradients? https://en.wikipedia.org/wiki/Gradient *)
+Lemma gradientP {R : realType} (n : nat) (f : 'rV[R]_n.+1 -> R^o) (v : 'rV[R]_n.+1) :
+  forall x : 'rV[R]_n.+1, (gradient f x) *d v = 'D_v f x.
+Proof.
+move=> x.
+rewrite /gradient.
+Admitted.
+
+Definition weakly_smooth_cond {R : realType} {n : nat} (a : 'rV[R]_n.+1) :=
+  let m := \big[minr/1(*def element*)]_i a``_i in
+  forall i j, i != j -> a``_i != m /\ a``_j != m.
+
+Definition weakly_smooth {R : realType} (n : nat) (f : 'rV[R]_n.+1 -> R) :=
+  (forall a, {for a, continuous f}) /\
+  (forall a, weakly_smooth_cond a -> {for a, continuous (gradient f)}).
+
+Definition product_and {R : fieldType} n (u : 'rV[R]_n) : R :=
+  \prod_(i < n) u ``_ i.
+
+Definition dl2_and {R : fieldType} n (v : 'rV[R]_n) :=
+  \sum_(i < n) v ``_ i.
+
+*)
