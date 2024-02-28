3799d622fb77545e7a96ad15cc9d3cae90bdf0ee
Alessandro Bruni <brun@itu.dk>
2024-02-28 15:45:20 +0100

    .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,15 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; see https://www.compart.com/en/unicode/U+xxxx
+
+((coq-mode . 
+  ((company-coq-dir-local-symbols .
+    ((":=" . ?≜) ("Proof." . ?∵) ("Qed." . ?■)
+     ("!=" . ?≠) ("*" . ?×) ("\\sum_" . ?∑) ("\\in" . ?∊) ("\\subset" . ?⊂)
+     ("`\\/" . ?∨) ("`/\\" . ?∧)
+     ("-[[" . ?⟦) ("[[" . ?⟦) ("]]" . ?⟧) ("{[" . ?⦃) ("]}" . ?⦄)
+     ("Defined." . ?□) ("Time" . ?⏱) ("Admitted." . ?😱)))
+   (prettify-symbols-mode . t)
+)))
+
