b3b5cc20db424d81ec0991ec40cb2e14a83b51e3
Alessandro Bruni <brun@itu.dk>
2025-08-27 07:43:35 +0200

    changed syntax and example1

--- example.v
+++ example.v
@@ -39,51 +39,13 @@ Local Notation expr := (@expr R).
 Let ldl_norm_infty n : expr (Fun_T n.+1 1) := ldl_fun
   (fun t : n.+1.-tuple R => [tuple \big[maxr/[tnth t 0] ]_(i <- t) i ])%R.
 
-Definition ldl_vec_sub n : expr (Vector_T n) -> expr (Vector_T n) ->
-  expr (Vector_T n).
-Proof.
-elim => {n}.
-- move=> _; exact.
-- move=> fn fi fm fl _; exact.
-- move=> n i; exact.
-- move=> n t.
-  elim.
-  + move=> _; exact: (ldl_real 0).
-  + move=> fn fi fm fl; exact: ldl_bool.
-  + move=> k; exact: ldl_idx.
-  + move=> l t2; exact: (ldl_vec [tuple nth 0 t i - nth 0 t2 i | i < l])%R.
-  + move=> fn fi fm _; exact: ldl_bool true.
-  + move=> fn fi fm _; exact: ldl_bool true.
-  + move=> fi fm fl e1 e2. exact: e1.
-  + move=> fn fm fl _ e2 _ _; exact: e2.
-  + move=> fn fi fl _; exact: ldl_bool true.
-  + move=> fn fi fl _; exact: ldl_bool true.
-  + move=> fn fi fm fl _ _ _ _ _; exact: ldl_bool true.
-  + move=> l k; exact: ldl_fun.
-  + move=> l k _ _ _ _; exact: (ldl_vec [tuple 0 | i < k])%R.
-  + move=> l _ _ _ _; exact: (ldl_real 0).
-- move=> fn fi fm _; exact.
-- move=> fn fi fm _; exact.
-- move=> fn fi fm _ _; exact.
-- move=> fn fi fm _ _ _ _ e1; exact: e1.
-- move=> fn fi fl _ e2; exact: e2.
-- move=> fn fi fl _; exact.
-- move=> fn fi fm fl _ _ _ _ _; exact.
-- move=> k m _ e; exact: e.
-- move=> n m e1 f1 e2 f2 e3; exact: (ldl_app e1 e2).
-- move=> _ _ _ _ _; exact.
-Defined.
+Program Definition ldl_vec_sub n :=
+  ldl_fun2 (fun (x y : n.-tuple R) => [tuple tnth x i - tnth y i | i < n]%R).
 
 Lemma ldl_vec_sub0 n (e : expr (Vector_T n)) :
-  ldl_vec_sub e (ldl_vec [tuple of nseq n 0%R]) = e.
+  [[ ldl_app2 (ldl_vec_sub n) e (ldl_vec [tuple of nseq n 0%R]) ]]_B = [[ e ]]_B.
 Proof.
-dependent induction e.
-- rewrite /ldl_vec_sub/=.
-  congr ldl_vec.
-  apply/eq_from_tnth => i.
-  rewrite tnth_mktuple nth_nseq ltn_ord subr0.
-  by apply: set_nth_default; rewrite size_tuple.
-- by [].
+by dependent induction e => /=; apply/eq_from_tnth => i; rewrite tnth_mktuple tnth_nseq subr0.
 Qed.
 
 Context {n m : nat} (eps delta : expr Real_T) (f : expr (Fun_T n.+1 m.+1))
@@ -91,9 +53,9 @@ Context {n m : nat} (eps delta : expr Real_T) (f : expr (Fun_T n.+1 m.+1))
 
 Definition eps_delta_robust fn fm fl : expr (Bool_T fn impl_def fm fl) :=
   ldl_impl
-    (ldl_lookup (ldl_app (ldl_norm_infty n) (ldl_vec_sub x v))
+    (ldl_lookup (ldl_app (ldl_norm_infty n) (ldl_app2 (ldl_vec_sub _) x v))
                 (ldl_idx ord0) `<= eps)
-    (ldl_lookup (ldl_app (ldl_norm_infty m) (ldl_vec_sub (ldl_app f x) (ldl_app f v)))
+    (ldl_lookup (ldl_app (ldl_norm_infty m) (ldl_app2 (ldl_vec_sub _) (ldl_app f x) (ldl_app f v)))
                 (ldl_idx ord0) `<= delta).
 
 End example_robust.
@@ -104,89 +66,20 @@ Context {R : realType}.
 
 Local Notation expr := (@expr R).
 
-Definition ldl_real_add : expr Real_T -> expr Real_T -> expr Real_T.
-Proof.
-elim.
-- move=> r _; exact: (ldl_real r).
-- move=> fn fi fm fl _; exact.
-- move=> m i; exact.
-- move=> m t1.
-  elim.
-  + move=> r; exact: (ldl_real r).
-  + move=> fn fi fm fl; exact: ldl_bool.
-  + move=> k; exact: ldl_idx.
-  + move=> l t2; exact: (ldl_vec [tuple nth 0 t1 i - nth 0 t2 i | i < l])%R.
-  + move=> fn fi fm _; exact: ldl_bool true.
-  + move=> fn fi fm _; exact: ldl_bool true.
-  + move=> fi fm fl e1 e2. exact: e1.
-  + move=> fn fm fl _ e2 _ _; exact: e2.
-  + move=> fn fi fl _; exact: ldl_bool true.
-  + move=> fn fi fl _; exact: ldl_bool true.
-  + move=> fn fi fm fl _ _ _ _ _; exact: ldl_bool true.
-  + move=> l k; exact: ldl_fun.
-  + move=> l k _ _ _ _; exact: (ldl_vec [tuple 0 | i < k])%R.
-  + move=> l _ _ _ _; exact: (ldl_real 0).
-- move=> fn fi fm _; exact.
-- move=> fn fi fm _; exact.
-- move=> fn fi fm _ _; exact.
-- move=> fn fi fm _ _ _ _ e1; exact: e1.
-- move=> fn fi fl _ e2; exact: e2.
-- move=> fn fi fl _; exact.
-- move=> fn fi fm fl _ _ _ _ _; exact.
-- move=> k m _ e; exact: e.
-- move=> _ m _ _ _ _; exact.
-- move=> n e1 f1 e2 f2 r; exact: ldl_lookup e1 e2.
-Defined.
+Definition ldl_real_add n :=
+  ldl_fun2 (fun (x y : n.-tuple R) => [tuple tnth x i + tnth y i | i < n]%R).
 
-Lemma ldl_real0 (e : expr Real_T) : ldl_real_add e (ldl_real 0) = e.
-Proof.
-dependent induction e.
-- by [].
-- by [].
-Qed.
-
-Definition ldl_real_sub : expr Real_T -> expr Real_T -> expr Real_T.
-Proof.
-elim.
-- move=> _; exact.
-- move=> fn fi fm fl _; exact.
-- move=> m i; exact.
-- move=> m t1.
-  elim.
-  + move=> _; exact: (ldl_real 0).
-  + move=> fn fi fm fl; exact: ldl_bool.
-  + move=> k; exact: ldl_idx.
-  + move=> l t2; exact: (ldl_vec [tuple nth 0 t1 i - nth 0 t2 i | i < l])%R.
-  + move=> fn fi fm _; exact: ldl_bool true.
-  + move=> fn fi fm _; exact: ldl_bool true.
-  + move=> fi fm fl e1 e2. exact: e1.
-  + move=> fn fm fl _ e2 _ _; exact: e2.
-  + move=> fn fi fl _; exact: ldl_bool true.
-  + move=> fn fi fl _; exact: ldl_bool true.
-  + move=> fn fi fm fl _ _ _ _ _; exact: ldl_bool true.
-  + move=> l k; exact: ldl_fun.
-  + move=> l k _ _ _ _; exact: (ldl_vec [tuple 0 | i < k])%R.
-  + move=> l _ _ _ _; exact: (ldl_real 0).
-- move=> fn fi fm _; exact.
-- move=> fn fi fm _; exact.
-- move=> fn fi fm _ _; exact.
-- move=> fn fi fm _ _ _ _ e1; exact: e1.
-- move=> fn fi fl _ e2; exact: e2.
-- move=> fn fi fl _; exact.
-- move=> fn fi fm fl _ _ _ _ _; exact.
-- move=> k m _ e; exact: e.
-- move=> _ m _ _ _ _; exact.
-- move=> _ _ _ _ _; exact.
-Defined.
+Definition ldl_real_sub n :=
+  ldl_fun2 (fun (x y : n.-tuple R) => [tuple tnth x i - tnth y i | i < n]%R).
 
 (*Notes:
 - does not say groups need to cover ALL indices*)
 
-Fixpoint ldl_sum_vec (x : seq (expr Real_T)) :=
-  match x with
-  | nil => ldl_real 0
-  | a::l => ldl_real_add a (ldl_sum_vec l)
-end.
+(* Fixpoint ldl_sum_vec (x : seq (expr Real_T)) := *)
+(*   match x with *)
+(*   | nil => ldl_real 0 *)
+(*   | a::l => ldl_real_add a (ldl_sum_vec l) *)
+(* end. *)
 
 Definition prob_group n m
     (f : expr (Fun_T n.+1 m.+1))
--- ldl.v
+++ ldl.v
@@ -80,9 +80,11 @@ Inductive flag_lattice := l_def | l_undef.
 Inductive ldl_type :=
 | Bool_T of flag_neg & flag_impl & flag_monoid & flag_lattice
 | Index_T of nat
-| Real_T
 | Vector_T of nat
-| Fun_T of nat & nat.
+| Fun_T of nat & nat
+| Fun2_T of nat & nat & nat.
+
+Definition Real_T := Vector_T 1.
 
 Definition Bool_T_undef := Bool_T neg_undef.
 Definition Bool_T_def := Bool_T neg_def.
@@ -99,7 +101,6 @@ Context {R : realType}.
 
 Inductive expr : ldl_type -> Type :=
   (* base expressions *)
-  | ldl_real : R -> expr Real_T
   | ldl_bool : forall p r s t, bool -> expr (Bool_T p r s t)
   | ldl_idx : forall n, 'I_n -> expr (Index_T n)
   | ldl_vec : forall n, n.-tuple R -> expr (Vector_T n)
@@ -115,7 +116,9 @@ Inductive expr : ldl_type -> Type :=
   | ldl_cmp : forall x y z v, comparison -> expr Real_T -> expr Real_T -> expr (Bool_T x y z v)
   (* networks and applications *)
   | ldl_fun : forall n m, (n.-tuple R -> m.-tuple R) -> expr (Fun_T n m)
+  | ldl_fun2 : forall n m l, (n.-tuple R -> m.-tuple R -> l.-tuple R) -> expr (Fun2_T n m l)
   | ldl_app : forall n m, expr (Fun_T n m) -> expr (Vector_T n) -> expr (Vector_T m)
+  | ldl_app2 : forall n m l, expr (Fun2_T n m l) -> expr (Vector_T n) -> expr (Vector_T m) -> expr (Vector_T l)
   | ldl_lookup : forall n, expr (Vector_T n) -> expr (Index_T n) -> expr Real_T.
 
 End expr.
@@ -151,7 +154,6 @@ Check expr_ind.
 
 Lemma expr_ind' (R : realType) :
   forall P : forall l : ldl_type, expr l -> Prop,
-       (forall s : R, P Real_T (ldl_real s)) ->
        (forall (p : flag_neg) (r : flag_impl) (s : flag_monoid) (t : flag_lattice) (b : bool),
         P (Bool_T p r s t) (ldl_bool p r s t b)) ->
        (forall (n : nat) (o : 'I_n), P (Index_T n) (ldl_idx o)) ->
@@ -174,19 +176,21 @@ Lemma expr_ind' (R : realType) :
           (c : comparison) (e : expr Real_T),
         P Real_T e -> forall e0 : expr Real_T, P Real_T e0 -> P (Bool_T x y z v) (ldl_cmp x y z v c e e0)) ->
        (forall (n m : nat) (t : n.-tuple R -> m.-tuple R), P (Fun_T n m) (ldl_fun t)) ->
+       (forall (n m l : nat) (t : n.-tuple R -> m.-tuple R -> l.-tuple R), P (Fun2_T n m l) (ldl_fun2 t)) ->
        (forall (n m : nat) (e : expr (Fun_T n m)),
         P (Fun_T n m) e -> forall e0 : expr (Vector_T n), P (Vector_T n) e0 -> P (Vector_T m) (ldl_app e e0)) ->
+       (forall (n m l : nat) (e : expr (Fun2_T n m l)),
+        P (Fun2_T n m l) e -> forall (e0 : expr (Vector_T n)) (e1 : expr (Vector_T m)), P (Vector_T n) e0 -> P (Vector_T m) e1 -> P (Vector_T l) (ldl_app2 e e0 e1)) ->
        (forall (n : nat) (e : expr (Vector_T n)),
         P (Vector_T n) e -> forall e0 : expr (Index_T n), P (Index_T n) e0 -> P Real_T (ldl_lookup e e0)) ->
        forall (l : ldl_type) (e : expr l), P l e.
 Proof.
-move => P H H0 H1 H2 H3 H4 H7 H11 H12 H13 H14 H15 H16 H17 s e.
+move => P H0 H1 H2 H3 H4 H7 H11 H12 H13 H14 H15 H16 H17 H18 H19 s e.
 revert e.
 revert s.
 fix F1 2.
 intros.
 destruct e.
-  * apply H.
   * apply H0.
   * apply H1.
   * apply H2.
@@ -224,6 +228,8 @@ destruct e.
   * apply H15; eauto.
   * apply H16; eauto.
   * apply H17; eauto.
+  * apply H18; eauto.
+  * apply H19; eauto.
 Qed.
 
 Local Close Scope ldl_scope.
@@ -236,28 +242,28 @@ Context {R : realType}.
 Definition type_translation (t : ldl_type) : Type:=
   match t with
   | Bool_T x y z v  => R
-  | Real_T => R
   | Vector_T n => n.-tuple R
   | Index_T n => 'I_n
   | Fun_T n m => n.-tuple R -> m.-tuple R
+  | Fun2_T n m l => n.-tuple R -> m.-tuple R -> l.-tuple R
 end.
 
 Definition bool_type_translation (t : ldl_type) : Type:=
   match t with
   | Bool_T x y z v=> bool
-  | Real_T => R
   | Vector_T n => n.-tuple R
   | Index_T n => 'I_n
   | Fun_T n m => n.-tuple R -> m.-tuple R
+  | Fun2_T n m l => n.-tuple R -> m.-tuple R -> l.-tuple R
   end.
 
 Definition ereal_type_translation (t : ldl_type) : Type :=
   match t with
   | Bool_T x y z v=> \bar R
-  | Real_T => R
   | Vector_T n => n.-tuple R
   | Index_T n => 'I_n
   | Fun_T n m => n.-tuple R -> m.-tuple R
+  | Fun2_T n m l => n.-tuple R -> m.-tuple R -> l.-tuple R
 end.
 
 End type_translation.
@@ -270,7 +276,6 @@ Context {R : realType}.
 Fixpoint bool_translation {t} (e : @expr R t) : bool_type_translation t :=
   match e in expr t return bool_type_translation t with
   | ldl_bool f1 f2 f3 f4 x => x
-  | ldl_real r => r%R
   | ldl_idx n i => i
   | ldl_vec n t => t
 
@@ -281,12 +286,14 @@ Fixpoint bool_translation {t} (e : @expr R t) : bool_type_translation t :=
   | ldl_mand f1 f2 f4  Es => \big[andb/true]_(i <- map bool_translation Es) i
   | ldl_mor f1 f2 f4  Es => \big[orb/false]_(i <- map bool_translation Es) i
 
-  | E1 `== E2 => << E1 >> == << E2 >>
-  | E1 `<= E2 => << E1 >> <= << E2 >>
+  | E1 `== E2 => tnth << E1 >> 0 == tnth << E2 >> 0
+  | E1 `<= E2 => tnth << E1 >> 0 <= tnth << E2 >> 0
 
   | ldl_fun n m f => f
+  | ldl_fun2 n m l f => f
   | ldl_app n m f v => << f >> << v >>
-  | ldl_lookup n v i => tnth << v >> << i >>
+  | ldl_app2 n m l f v1 v2 => << f >> << v1 >> << v2 >>
+  | ldl_lookup n v i => mktuple (fun => tnth << v >> << i >>)
   end
 where "<< e >>" := (bool_translation e).
 
@@ -342,10 +349,9 @@ Context {R : realType}.
 Variables (l : DL) (p : R).
 
 Fixpoint translation {t} (e : @expr R t) {struct e} : type_translation t :=
-   match e in expr t return type_translation t with
+  match e in expr t return type_translation t with
    | ldl_bool _ _ _ _ true => (1%R : type_translation (Bool_T _ _ _ _))
    | ldl_bool _ _ _ _ false => (0%R : type_translation (Bool_T _ _ _ _ ))
-   | ldl_real r => r%R
    | ldl_idx n i => i
    | ldl_vec n t => t
 
@@ -371,46 +377,47 @@ Fixpoint translation {t} (e : @expr R t) {struct e} : type_translation t :=
        | GodelS => maxR (map translation Es)
        | productS => product_dl_prod (map translation Es)
        end
-    (*| `~ E1 => 1 - {[ E1 ]}*)
-    | ldl_not _ _ _ E1 =>
-       match l with
-       | Lukasiewicz => 1 - {[ E1 ]}
-       | Yager => 1 - {[ E1 ]}
-       | Godel => if {[ E1 ]} > 0 then 0 else 1
-       | product => if {[ E1 ]} > 0 then 0 else 1
-       | GodelS => 1 - {[ E1 ]}
-       | productS => 1 - {[ E1 ]}
-       end
+  (*| `~ E1 => 1 - {[ E1 ]}*)
+  | ldl_not _ _ _ E1 =>
+      match l with
+      | Lukasiewicz => 1 - {[ E1 ]}
+      | Yager => 1 - {[ E1 ]}
+      | Godel => if {[ E1 ]} > 0 then 0 else 1
+      | product => if {[ E1 ]} > 0 then 0 else 1
+      | GodelS => 1 - {[ E1 ]}
+      | productS => 1 - {[ E1 ]}
+      end
 
-    | ldl_impl _ _ _ E1 E2 =>
-        match l with
-       | Lukasiewicz => minr (1 - {[ E1 ]} + {[ E2 ]}) 1
-       | Yager => minr (((1 - {[ E1 ]})`^p + ({[ E2 ]})`^p )`^p^-1) 1
-       | Godel => if {[ E2 ]} < {[ E1 ]} then {[ E2 ]} else 1
-       | product => if {[ E2 ]} < {[ E1 ]} then
-                      {[ E2 ]} / {[ E1 ]}
-                    else
-                      1
-       | GodelS => maxr (1 - {[ E1 ]}) {[ E2 ]}
-       | productS => 1 - ( 1 - {[ E2 ]}) * {[ E1 ]}
-       end
-
-    | E1 `== E2 => if {[ E1 ]} == - {[ E2 ]} then
-                     ({[ E1 ]} == {[ E2 ]})%:R
+  | ldl_impl _ _ _ E1 E2 =>
+      match l with
+      | Lukasiewicz => minr (1 - {[ E1 ]} + {[ E2 ]}) 1
+      | Yager => minr (((1 - {[ E1 ]})`^p + ({[ E2 ]})`^p )`^p^-1) 1
+      | Godel => if {[ E2 ]} < {[ E1 ]} then {[ E2 ]} else 1
+      | product => if {[ E2 ]} < {[ E1 ]} then
+                     {[ E2 ]} / {[ E1 ]}
                    else
-                     maxr (1 - `|({[ E1 ]} - {[ E2 ]}) / ({[ E1 ]} + {[ E2 ]})|)
-                          0
-    | E1 `<= E2 => if {[ E1 ]} == - {[ E2 ]} then
-                     ({[ E1 ]} <= {[ E2 ]})%R%:R
-                   else
-                     maxr (1 - maxr (({[ E1 ]} - {[ E2 ]}) /
-                                      `|{[ E1 ]} + {[ E2 ]}|) 0)
-                          0
+                     1
+      | GodelS => maxr (1 - {[ E1 ]}) {[ E2 ]}
+      | productS => 1 - ( 1 - {[ E2 ]}) * {[ E1 ]}
+      end
 
-    | ldl_fun n m f => f
-    | ldl_app n m f v => (translation f) (translation v)
-    | ldl_lookup n v i => tnth (translation v) (translation i)
-    end
+  | E1 `== E2 =>
+      let e1 := tnth (translation E1) 0 in
+      let e2 := tnth (translation E2) 0 in
+      if e1 == - e2 then (e1 == e2)%:R
+      else maxr (1 - `|(e1 - e2) / (e1 + e2)|) 0
+  | E1 `<= E2 =>
+      let e1 := tnth (translation E1) 0 in
+      let e2 := tnth (translation E2) 0 in
+      if e1 == - e2 then (e1 <= e2)%R%:R
+      else maxr (1 - maxr ((e1 - e2) / `|e1 + e2|) 0) 0
+
+  | ldl_fun n m f => f
+  | ldl_fun2 n m l f => f
+  | ldl_app n m f v => {[ f ]} {[ v ]}
+  | ldl_app2 n m l f v1 v2 => {[ f ]} {[ v1 ]} {[ v2 ]}
+  | ldl_lookup n v i => mktuple (fun=> tnth {[ v ]} {[ i ]})
+  end
 where "{[ e ]}" := (translation e).
 
 End fuzzy_translation.
@@ -424,7 +431,6 @@ Fixpoint dl2_ereal_translation {t} (e : @expr R t) {struct e} : ereal_type_trans
   match e in expr t return ereal_type_translation t with
   | ldl_bool _ _ _ _ true => 0
   | ldl_bool _ _ _ _ false => -oo
-  | ldl_real r => r
   | ldl_idx n i => i
   | ldl_vec n t => t
   | ldl_and _ _ _ Es => +oo (* default value, all lemmas are for negation-free formulas *)
@@ -446,12 +452,14 @@ Fixpoint dl2_ereal_translation {t} (e : @expr R t) {struct e} : ereal_type_trans
   | ldl_not _ _ _ E1 => +oo (* default value, all lemmas are for negation-free formulas *)
   | ldl_impl _ _ _ E1 E2 =>  (- maxe ({[ E1 ]} - {[ E2 ]}) 0)
                                 
-  | E1 `== E2 => (- `| {[ E1 ]} - {[ E2 ]}|)%:E
-  | E1 `<= E2 => (- maxr ({[ E1 ]} - {[ E2 ]}) 0)%:E
+  | E1 `== E2 => (- `| tnth {[ E1 ]} 0 - tnth {[ E2 ]} 0|)%:E
+  | E1 `<= E2 => (- maxr (tnth {[ E1 ]} 0 - tnth {[ E2 ]} 0) 0)%:E
 
   | ldl_fun n m f => f
+  | ldl_fun2 n m l f => f
   | ldl_app n m f v => {[ f ]} {[ v ]}
-  | ldl_lookup n v i => tnth {[ v ]} {[ i ]}
+  | ldl_app2 n m l f v1 v2 => {[ f ]} {[ v1 ]} {[ v2 ]}
+  | ldl_lookup n v i => mktuple (fun => tnth {[ v ]} {[ i ]})
   end
 where "{[ e ]}" := (dl2_ereal_translation e).
 
@@ -467,7 +475,6 @@ Fixpoint dl2_translation {t} (e : @expr R t) {struct e} : type_translation t :=
   match e in expr t return type_translation t with
   | ldl_bool _ _ _ _ true => 0
   | ldl_bool _ _ _ _ false => -1
-  | ldl_real r => r
   | ldl_idx n i => i
   | ldl_vec n t => t
 
@@ -479,12 +486,14 @@ Fixpoint dl2_translation {t} (e : @expr R t) {struct e} : type_translation t :=
   | ldl_not _ _ _ E1 => 0 (* default value, all lemmas are for negation-free formulas *)
   | ldl_impl _ _ _ E1 E2 => (- maxr ({[ E1 ]} - {[ E2 ]}) 0)
 
-  | E1 `== E2 => (- `| {[ E1 ]} - {[ E2 ]}|)
-  | E1 `<= E2 => (- maxr ({[ E1 ]} - {[ E2 ]}) 0)
+  | E1 `== E2 => (- `| tnth {[ E1 ]} 0 - tnth {[ E2 ]} 0|)
+  | E1 `<= E2 => (- maxr (tnth {[ E1 ]} 0 - tnth {[ E2 ]} 0) 0)
 
   | ldl_fun n m f => f
+  | ldl_fun2 n m l f => f
   | ldl_app n m f v => {[ f ]} {[ v ]}
-  | ldl_lookup n v i => tnth {[ v ]} {[ i ]}
+  | ldl_app2 n m l f v1 v2 => {[ f ]} {[ v1 ]} {[ v2 ]}
+  | ldl_lookup n v i => mktuple (fun => tnth {[ v ]} {[ i ]})
 end
 where "{[ e ]}" := (dl2_translation e).
 
@@ -510,7 +519,6 @@ Fixpoint stl_ereal_translation {t} (e : expr t) : ereal_type_translation t :=
   match e in expr t return ereal_type_translation t with
   | ldl_bool _ _ _ _ true => +oo
   | ldl_bool _ _ _ _ false => -oo
-  | ldl_real r => r
   | ldl_idx n i => i
   | ldl_vec n t => t
 
@@ -546,12 +554,14 @@ Fixpoint stl_ereal_translation {t} (e : expr t) : ereal_type_translation t :=
   | ldl_impl _ _ _ E1 E2 => 0 (* default value, all lemmas are for implication-free formulas *)
 
   (*comparisons*)
-  | E1 `== E2 => (- `| {[ E1 ]} - {[ E2 ]}|)%:E
-  | E1 `<= E2 => ({[ E2 ]} - {[ E1 ]})%:E(* (- maxr ({[ E1 ]} - {[ E2 ]}) 0)%:E *)
+  | E1 `== E2 => (- `| tnth {[ E1 ]} 0 - tnth {[ E2 ]} 0|)%:E
+  | E1 `<= E2 => (tnth {[ E2 ]} 0 - tnth {[ E1 ]} 0)%:E(* (- maxr ({[ E1 ]} - {[ E2 ]}) 0)%:E *)
 
   | ldl_fun n m f => f
+  | ldl_fun2 n m l f => f
   | ldl_app n m f v => {[ f ]} {[ v ]}
-  | ldl_lookup n v i => tnth {[ v ]} {[ i ]}
+  | ldl_app2 n m l f v1 v2 => {[ f ]} {[ v1 ]} {[ v2 ]}
+  | ldl_lookup n v i => mktuple (fun => tnth {[ v ]} {[ i ]})
     end
 where "{[ e ]}" := (stl_ereal_translation e).
 
@@ -617,7 +627,6 @@ Fixpoint stl_translation {t} (e : expr t) : type_translation t :=
   match e in expr t return type_translation t with
   | ldl_bool _ _ _ _ true => 1
   | ldl_bool _ _ _ _ false => -1
-  | ldl_real r => r
   | ldl_idx n i => i
   | ldl_vec n t => t
 
@@ -638,12 +647,14 @@ Fixpoint stl_translation {t} (e : expr t) : type_translation t :=
   | `~ E1 => - {[ E1 ]}
   | E1 `=> E2 => 0 (* default value, all lemmas are for negation-free formulas *)
 
-  | E1 `== E2 => - `| {[ E1 ]} - {[ E2 ]}|
-  | E1 `<= E2 => {[ E2 ]} - {[ E1 ]}
+  | E1 `== E2 => - `| tnth {[ E1 ]} 0 - tnth {[ E2 ]} 0|
+  | E1 `<= E2 => tnth {[ E2 ]} 0 - tnth {[ E1 ]} 0
 
   | ldl_fun n m f => f
+  | ldl_fun2 n m l f => f
   | ldl_app n m f v => {[ f ]} {[ v ]}
-  | ldl_lookup n v i => tnth {[ v ]} {[ i ]}
+  | ldl_app2 n m l f v1 v2 => {[ f ]} {[ v1 ]} {[ v2 ]}
+  | ldl_lookup n v i => mktuple (fun => tnth {[ v ]} {[ i ]})
   end
 where "{[ e ]}" := (stl_translation e).
 
