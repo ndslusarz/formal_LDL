0901a0a6b6d24b7ce6e3a79c529ccdaca94a08e8
ndslusarz <nds1@hw.ac.uk>
2025-08-28 14:37:57 +0100

    some axioms wip

--- fuzzy.v
+++ fuzzy.v
@@ -829,6 +829,32 @@ rewrite /= !big_cons big_nil !addr0.
 by rewrite/minr; case: ifP; intros; lra.
 Qed.
 
+Lemma Lukasiewicz_prelinearity (e1 e2 e3 : expr Bool_T_fuzzy) :
+  [[e1 `** e2]]_Lukasiewicz <= [[ e3 ]]_Lukasiewicz <-> [[ e2 ]]_Lukasiewicz <= [[e1 `=> e3]]_Lukasiewicz.
+Proof.
+have h1 := translate_Bool_T_01 p p1 Lukasiewicz _ _ _ e1.
+have h2 := translate_Bool_T_01 p p1 Lukasiewicz _ _ _ e2.
+have h3 := translate_Bool_T_01 p p1 Lukasiewicz _ _ _ e3.
+split; rewrite//= !big_cons big_nil addr0 /minr/maxr; repeat case: ifP; intros; lra.
+Qed.
+
+Lemma Lukasiewicz_involution (e : expr Bool_T_fuzzy) :
+  [[`~ (`~e)]]_Lukasiewicz = [[ e ]]_Lukasiewicz.
+Proof. by rewrite//=; lra. Qed.
+
+
+Lemma Lukasiewicz_demorgan_mand  (e1 e2 : expr Bool_T_fuzzy) :
+  [[`~ (e1 `** e2)]]_Lukasiewicz = [[(`~ e1) `++ (`~ e2)]]_Lukasiewicz.
+Proof.
+rewrite//= !big_cons big_nil !addr0 /maxr /minr; repeat case: ifP; intros; lra.
+Qed.
+
+Lemma Lukasiewicz_demorgan_mor  (e1 e2 : expr Bool_T_fuzzy) :
+  [[`~ (e1 `++ e2)]]_Lukasiewicz = [[(`~ e1) `** (`~ e2)]]_Lukasiewicz.
+Proof.
+rewrite//= !big_cons big_nil !addr0 /maxr /minr; repeat case: ifP; intros; lra.
+Qed.
+
 End Lukasiewicz_lemmas.
 
 Section Yager_lemmas.
@@ -1062,6 +1088,35 @@ rewrite -powRrM divff//= ?powRr1 ?p_nq//=; try lra.
 rewrite /minr; case: ifP; move => hy; lra.
 Qed.
 
+(*Lemma Yager_prelinearity (e1 e2 e3 : expr Bool_T_fuzzy) :
+  [[e1 `** e2]]_Yager <= [[ e3 ]]_Yager <-> [[ e2 ]]_Yager <= [[e1 `=> e3]]_Yager.
+Proof.
+have := translate_Bool_T_01 p p1 Yager _ _ _ e1.
+have := translate_Bool_T_01 p p1 Yager _ _ _ e2.
+have := translate_Bool_T_01 p p1 Yager _ _ _ e3.
+split; rewrite//= !big_cons big_nil !addr0 /maxr/minr; case: ifP;
+  set x := (1 - [[e1]]_Yager) `^ p; 
+  set y := (1 - [[e2]]_Yager) `^ p;
+  set z := [[e3]]_Yager; intros; case: ifP => H'.
+- move: H'. set xx := (x + (z `^ p)%R)%E `^ p^-1. lra.
+
+Lemma Lukasiewicz_involution (e : expr Bool_T_fuzzy) :
+  [[`~ (`~e)]]_Yager = [[ e ]]_Yager_Lukasiewicz.
+Proof. by rewrite//=; lra. Qed.
+
+
+Lemma Lukasiewicz_demorgan_mand  (e1 e2 : expr Bool_T_fuzzy) :
+  [[`~ (e1 `** e2)]]_Lukasiewicz = [[(`~ e1) `++ (`~ e2)]]_Lukasiewicz.
+Proof.
+rewrite//= !big_cons big_nil !addr0 /maxr /minr; repeat case: ifP; intros; lra.
+Qed.
+
+Lemma Lukasiewicz_demorgan_mor  (e1 e2 : expr Bool_T_fuzzy) :
+  [[`~ (e1 `++ e2)]]_Lukasiewicz = [[(`~ e1) `** (`~ e2)]]_Lukasiewicz.
+Proof.
+rewrite//= !big_cons big_nil !addr0 /maxr /minr; repeat case: ifP; intros; lra.
+Qed*)
+
 End Yager_lemmas.
 
 Section Godel_lemmas.
@@ -1153,6 +1208,14 @@ Proof.
 have := translate_Bool_T_01 p p1 Godel _ _ _ e.
 rewrite//= /maxR !big_cons big_nil.
 rewrite /maxr; repeat case: ifP; intros; lra.
+Qed.
+
+Lemma Godel_prelinearity (e1 e2 e3 : expr Bool_T_fuzzy) :
+  [[e1 `** e2]]_Godel <= [[ e3 ]]_Godel <-> [[ e2 ]]_Godel <= [[e1 `=> e3]]_Godel.
+Proof.
+have := translate_Bool_T_01 p p1 Godel _ _ _ e1.
+have := translate_Bool_T_01 p p1 Godel _ _ _ e2.
+split; rewrite//=/minR; rewrite !big_cons big_nil /minr; repeat case: ifP; intros; try lra.
 Qed.
 
 End Godel_lemmas.
